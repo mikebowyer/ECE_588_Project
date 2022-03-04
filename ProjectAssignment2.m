9dd45a6070d257c60eb88c41be48aca166e9ccaa
Michael Bowyer <mbowyer@umich.edu>
2022-03-03 21:20:46 -0500

    First trial commit

--- ProjectAssignment2.m
+++ ProjectAssignment2.m
@@ -1,20 +1,25 @@
-ip_TurtleBot = '10.0.1.57';    
-ip_Matlab = '10.0.1.54';      
+rosshutdown
+
+ip_TurtleBot = '127.0.0.1';    
+ip_Matlab = '127.0.0.1';      
 
 setenv('ROS_MASTER_URI', strcat('http://', ip_TurtleBot,':11311'))
 setenv('ROS_IP', ip_Matlab)
 
 rosinit(ip_TurtleBot)
+%% Ros Topic Subscriptions and Publications
+[cmd_vel_pub,twist_msg] = rospublisher('/cmd_vel','geometry_msgs/Twist');
 %%
 
-rostopic info /raspicam_node/image/compressed
-msg_camera = rostopic("echo", "/raspicam_node/image/compressed")
-TurtleBot_Topic.picam = '/raspicam_node/image/compressed';
+rostopic info /camera/rgb/image_raw/compressed
+msg_camera = rostopic("echo", "camera/rgb/image_raw/compressed")
+TurtleBot_Topic.picam = 'camera/rgb/image_raw/compressed';
 image_sub = rossubscriber(TurtleBot_Topic.picam);
 image_compressed = receive(image_sub);
 %%
 %%% to display a continuously updating image from Pi camera
 tic;
+prev_time = now
 while toc < 20  
     %%% Edge detector
     %%% Takes raw image and isolated the line
@@ -41,4 +46,24 @@ while toc < 20
     %Determine if we are on the line, and if not get back on the line, then
     %follow line
     %will require an object to store the state
+    line_angle = 0 % Angle from -90 = horzontal left, 90 = horizontal right
+    intercept = 0 % 0 in middle, 50% at right edge, -50% at left edge
+
+    target_ang=0
+    target_intercept = 0
+
+    % Error Calc
+    err_angle = line_angle - target_ang
+    err_intercept = intercept - target_intercept
+
+    % delta time calc
+    current_time = now
+    time_delta = current_time - prev_time
+    
+    %
+
+    twist_msg.Linear.X = .1
+    twist_msg.Angular.Z = .25
+    send(cmd_vel_pub,twist_msg);
+
 end
