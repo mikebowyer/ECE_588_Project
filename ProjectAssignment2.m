45cdad78fbbcf8b1651bb46ba6c2ac4d304db9f2
Michael Bowyer <mbowyer@umich.edu>
2022-03-07 18:30:20 -0500

    Working robot control

--- ProjectAssignment2.m
+++ ProjectAssignment2.m
@@ -5,34 +5,72 @@ setenv('ROS_MASTER_URI', strcat('http://', ip_TurtleBot,':11311'))
 setenv('ROS_IP', ip_Matlab)
 
 rosinit(ip_TurtleBot)
-%%
-
+%% Setup Topic Subscriptions and Publications
+% Subscriptions
 TurtleBot_Topic.picam = "/camera/rgb/image_raw/compressed";
 %TurtleBot_Topic.picam = "/raspicam_node/image/compressed";
 image_sub = rossubscriber(TurtleBot_Topic.picam);
-%%
-%%% to display a continuously updating image from Pi camera
-%%
+% Publications
+[cmd_vel_pub,twist_msg] = rospublisher('/cmd_vel','geometry_msgs/Twist');
+
+%% Run Main Robot Control Loop
 close all
-figure, hold on
+figure('units','normalized','outerposition',[0 0 1 1]), hold on
 while true 
     % Get, rotate, and crop image
     image = grabAndCleanUpImage(image_sub);
     [img_height, img_width]= imageSizeFinder(image);
+
     % Hough Transform to Get Lines
     lines = IsolateGroundLines(image);
     PlotGroundLines(image, lines);
+
     % Getting Line of Best Fit from Hough Lines
-    coefficients = BestFitLine(lines);
-    PlotBestFitLine(coefficients)
-    % Converting line of best fit coeffiencts to intercept and slope
-    theta = convertLineOfBestFit(coefficients)
-    pixels_from_img_center = findInterceptFromImgCenter(coefficients, img_height, img_width)
+    if length(lines) == 0
+        continue
+    end
+    lineBestFitPoints = BestFitLineAvg(lines);
+
+    % Converting line of best fit to intercept and slope
+    [theta, intercept, extent_points] = calcBestFitLineInfo(lineBestFitPoints, img_height);
+    PlotInterceptTheta(extent_points, intercept,theta, image)
+    
     % Control the boto
-    %sendControlMsg(intercept, theta)
+    twist_msg = calcCmdVelMsg(intercept, theta, twist_msg, img_width);
+    send(cmd_vel_pub,twist_msg);
 end
      
 %%
+function [theta, intercept, points] = calcBestFitLineInfo(lineBestFitPoints, img_height)
+    rise  = lineBestFitPoints(1,2) - lineBestFitPoints(2,2);
+    run = lineBestFitPoints(1,1) - lineBestFitPoints(2,1);
+    slope = rise / run;
+
+    theta = rad2deg(atan(slope));
+    if theta > 0
+        theta = 90 - theta;
+    else
+        theta = -90 - theta;
+    end
+
+    mx_b_intercept = lineBestFitPoints(1,2) - slope * lineBestFitPoints(1,1);
+
+    y1 = 1;
+    x1 = (1 -mx_b_intercept) / slope;
+    y2 = img_height;
+    x2 = (img_height -mx_b_intercept) / slope;
+    
+    intercept = x2;
+    points = [x1 y1; x2 y2];
+
+end
+function PlotInterceptTheta(lineBestFitPoints, intercept,theta, image)
+    subplot(224);
+    imshow(image);
+    hold on; 
+    plot(lineBestFitPoints(:,1),lineBestFitPoints(:,2),'LineWidth',2,'Color','red');
+    title("Intercept Pixel: " + ceil(intercept) + "   Theta: " + theta)
+end
 
 function image = grabAndCleanUpImage(image_sub)
     image_compressed = receive(image_sub);
@@ -45,24 +83,22 @@ function lines = IsolateGroundLines(image)
     cannyImage = edge(rgb2gray(image), 'canny');
     subplot(221); imshow(image)
     subplot(222); imshow(cannyImage)
-    subplot(223); imshow(image)
-    subplot(224); imshow(image)
-    [H, T, R] = hough(cannyImage, 'Theta', -45:0.5:45);
-    P = houghpeaks(H,20, 'Theta', 0:0.5:80);
-    lines = houghlines(cannyImage,T,R,P,'FillGap', 15, 'MinLength', 50);
+    [H, T, R] = hough(cannyImage, 'Theta', -60:0.5:60);
+    P = houghpeaks(H, 4, 'Theta', -60:0.5:60);
+    lines = houghlines(cannyImage,T,R,P,'FillGap', 50, 'MinLength', 80);
     
 end
 
 function PlotGroundLines(image, lines)
-    imshow(image), hold on
-    max_len = 0;
+    
+    subplot(223); imshow(image); hold on;
     for k = 1:length(lines)
        xy = [lines(k).point1; lines(k).point2];
        subplot(223);plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
     
        % Plot beginnings and ends of lines
-       subplot(223);plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
-       subplot(223);plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+       %subplot(223);plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+       %subplot(223);plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
     
        % Determine the endpoints of the longest line segment
 %        len = norm(lines(k).point1 - lines(k).point2);
@@ -82,10 +118,10 @@ function DetermineRoughlyParallelLines(colorImage, lines)
             if j ~= k
                 if lines(j).theta >= (lines(k).theta - 5) & lines(j).theta <= (lines(k).theta + 5)
                     %lines are roughly parallel, get average lines of two
-                    averageLinePoint1x = round(mean(lines(j).point1(1), lines(k).point1(1)))
-                    averageLinePoint1y = round(mean(lines(j).point1(2), lines(k).point1(2)))
-                    averageLinePoint2x = round(mean(lines(j).point2(1), lines(k).point2(1)))
-                    averageLinePoint2y = round(mean(lines(j).point2(2), lines(k).point2(2)))
+                    averageLinePoint1x = round(mean(lines(j).point1(1), lines(k).point1(1)));
+                    averageLinePoint1y = round(mean(lines(j).point1(2), lines(k).point1(2)));
+                    averageLinePoint2x = round(mean(lines(j).point2(1), lines(k).point2(1)));
+                    averageLinePoint2y = round(mean(lines(j).point2(2), lines(k).point2(2)));
                     %create line between two points
                     plot(averageLinePoint1x,averageLinePoint1y,'x','LineWidth',2,'Color','yellow');
                     plot(averageLinePoint2x,averageLinePoint2y,'x','LineWidth',2,'Color','red');
@@ -107,55 +143,71 @@ function coefficients = BestFitLine(lines)
                 xy = cat(1,xy,xyNew);
             end
         end
-        coefficients = polyfit(xy(:,1),xy(:,2), 1);
+        coefficients = polyfit(xy(:,1),-xy(:,2), 1);
     end
 end
 
-function PlotBestFitLine(coefficients)
-    y1 = polyval(coefficients,1);
-    y2 = polyval(coefficients,640);
-    hold on
-    subplot(224);plot([1, 640],[y1, y2],'LineWidth',2,'Color','red');
+function points = BestFitLineAvg(lines)
+    startPoints = [];
+    endPoints = [];
+    
+    if length(lines) ~= 0
+        for k = 1:length(lines)
+            startPoints = [startPoints ; lines(k).point1];
+            endPoints = [endPoints ; lines(k).point2];
+        end
+
+        startPointX = mean(startPoints(:,1));
+        startPointY = mean(startPoints(:,2));
+
+        endPointX = mean(endPoints(:,1));
+        endPointY = mean(endPoints(:,2));
+
+        points = [startPointX startPointY; endPointX endPointY];
+
+    end
 end
 
-%
 
-function sendControlMsg(intercept, theta)
-
+function twist_out = calcCmdVelMsg(intercept_pixel, theta, twist_in, img_width)
+    twist_out = twist_in;
+    
+    ratio_intercept_from_img_center = (intercept_pixel - (img_width/2)) / (img_width/2);
+    
+    max_turn_z_val = .5;
+    
+    twist_out.Linear.X = .5;
+    twist_out.Angular.Z = max_turn_z_val * (-theta/90) * (-(1 - ratio_intercept_from_img_center)*1.5);
+    twist_out.Angular.Z
 end
-
-% Kay's addition functions
-
-%% Function Definitions
 
 % returns angle in degrees of robot path line 
 function theta = convertLineOfBestFit(coefficients) 
-    theta = 0; % initialize angle variable
-    vect1 = [1 coefficients(1)]; % create a vector based on the line equation slope
-    vect2 = [1 0]; % create horizontal line vector 
-    dp = dot(vect1, vect2);
-    
-    % compute vector lengths
-    length1 = sqrt(sum(vect1.^2));
-    length2 = sqrt(sum(vect2.^2));
-    
-    % obtain the smaller angle of intersection in degrees
-    theta = acos(dp/(length1*length2))*180/pi;
+    theta = rad2deg(atan(-coefficients(1)))
+    if theta > 0
+        theta = 90 - theta
+    else
+        theta = -90 - theta
+    end
+%     theta = 0; % initialize angle variable
+%     vect1 = [1 coefficients(1)]; % create a vector based on the line equation slope
+%     vect2 = [1 0]; % create horizontal line vector 
+%     dp = dot(vect1, vect2);
+%     
+%     % compute vector lengths
+%     length1 = sqrt(sum(vect1.^2));
+%     length2 = sqrt(sum(vect2.^2));
+%     
+%     % obtain the smaller angle of intersection in degrees
+%     theta = acos(dp/(length1*length2))*180/pi;
 
 end
 
 % returns offset via x-intercept with hottom horizontal edge of image
 function pixels_from_img_center = findInterceptFromImgCenter(coefficients, img_height, img_width)
-    %x = [0:1:250];
     y = img_height;
-    % plot the line of robot path given coefficents
     x_intercept = (y - coefficients(2)) / coefficients(1);
     pixels_from_img_center = x_intercept - (img_width /2);
-    %hold on, subplot(2,1,2) 
-    %plot(y,x)
-    %xlim([0 600])
-    %ylim([0 250])
-    %x_intercept = y(1); %first index value 
 
 end
 
